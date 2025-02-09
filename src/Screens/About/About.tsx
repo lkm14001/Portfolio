804f58e84abe6ffec9ce0bb9f53b6069985fd957
Leela Krishna <krishnaleela35@gmail.com>
2025-02-09 22:52:25 +0530

    Added Border Customisations from Home Screen and about section

--- src/Screens/About/About.tsx
+++ src/Screens/About/About.tsx
@@ -7,131 +7,61 @@ import {
   useTransform,
 } from "motion/react";
 import Layout from "../Layout/Layout";
-import { Box, SxProps, Theme, Typography } from "@mui/material";
+import { Box, BoxProps, styled, SxProps, Theme, Typography } from "@mui/material";
 import me from "../../assets/me.jpg";
-import { TbBikeFilled } from "react-icons/tb";
-import React, { useRef } from "react";
+import React from "react";
+import Reveal from "../../components/Reveal/Reveal";
+import { LeftBorderLine, RightBorderLine } from "../Home/Home";
+import Marquee from 'react-fast-marquee'
 
 interface IGradientText {
   sx?: SxProps<Theme>;
   children: React.ReactNode;
 }
 
-interface IGradientBoxMotion extends IGradientText {
-  motionStyle?: MotionStyle;
-  id?: string;
-  animation?: AnimationProps;
-}
-
 const GradientText = ({ sx, children }: IGradientText) => {
   return (
     <Typography
       component="span"
-      sx={(theme) => ({
-        background:
-          "linear-gradient(90deg, rgba(247,119,15,1) 0%, rgba(232,66,15,1) 34%, rgba(246,0,232,1) 100%)",
-        color: "transparent",
-        backgroundClip: "text",
-        fontFamily: "inherit",
-        fontSize: "inherit",
-        fontWeight: "bolder",
-        "-webkit-background-clip": "text",
-        "-webkit-text-fill-color": "transparent",
-        sx,
-      })}
+      sx={[
+        {
+          background:
+            "linear-gradient(90deg, rgba(247,119,15,1) 0%, rgba(232,66,15,1) 34%, rgba(246,0,232,1) 100%)",
+          color: "transparent",
+          backgroundClip: "text",
+          fontFamily: "inherit",
+          fontSize: "inherit",
+          fontWeight: "bolder",
+          "-webkit-background-clip": "text",
+          "-webkit-text-fill-color": "transparent",
+        },
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
     >
       {children}
     </Typography>
   );
 };
 
-const GradientBoxMotion = ({
-  sx,
-  motionStyle,
-  children,
-  id,
-}: IGradientBoxMotion) => {
-  const BoxMotion = motion(Box);
-
-  const gridCellVariant = {
-    hidden: {
-      opacity: 0,
-      y: 50,
-      scale: 0.9,
-    },
-    visible: {
-      opacity: 1,
-      y: 0,
-      scale: 1,
-      transition: {
-        type: "spring",
-        duration: 0.8,
-      },
-    },
-  };
-
-  return (
-    <BoxMotion
-      id={id}
-      sx={[
-        {
-          background:
-            "linear-gradient(338deg, rgba(0,0,0,0.3) 0%, rgba(21,20,24,0.3) 15%, rgba(37,37,42,0.3) 37%, rgba(37,37,40,0.3) 57%, rgba(37,39,41,0.3) 87%, rgba(37,42,45,0.3) 100%)",
-          borderRadius: 3,
-          backdropFilter: "blur(5px)",
-          "-webkit-backdrop-filter": "blur(5px)",
-          borderWidth: 2,
-          borderColor: "#191919",
-          borderStyle: "solid",
-        },
-        ...(Array.isArray(sx) ? sx : [sx]),
-      ]}
-      style={motionStyle}
-      variants={gridCellVariant}
-      initial="hidden"
-      animate={"visible"}
-    >
-      {children}
-    </BoxMotion>
-  );
-};
+const CustomMarquee = styled(Marquee)<BoxProps>(({theme}) => ({}));
 
 const About = () => {
   const BoxMotion = motion(Box);
   const { scrollY } = useScroll();
   const opacity = useTransform(scrollY, [600, 800], [0, 1]);
   const y = useTransform(scrollY, [600, 700], [100, 0]);
-  const ref = useRef(null);
-  const isInView = useInView(ref, {
-    once: false,
-    amount: 0.2,
-    margin: "-100px",
-  });
-
-  const gridVariant = {
-    hidden: { opacity: 1 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.1,
-        delayChildren: 0.3,
-      },
-    },
-  };
 
   return (
     <Layout
-      sx={
-        {
-          // paddingTop: '10vh',
-          // position: "relative",
-        }
-      }
+      sx={{
+        // width: "100%",
+        // height: "100vh",
+        position: "relative",
+      }}
       id="about"
       motionStyle={{ opacity, y }}
     >
       <BoxMotion
-        ref={ref}
         style={{ opacity, y }}
         sx={(theme) => ({
           backgroundColor: "homePage.background",
@@ -139,72 +69,122 @@ const About = () => {
           minHeight: "100vh",
           boxSizing: "border-box",
           zIndex: 10,
-          display: "grid",
-          gridTemplateColumns: "repeat(5, 1fr)",
-          // gridTemplateRows: "repeat(3, 1fr)",
+          display: "flex",
+          alignItems: "center",
           gap: 2,
-          p: 15,
+          px: 10,
+          py: 5,
+          flexDirection: "column",
         })}
-        variants={gridVariant}
-        initial="hidden"
-        animate={isInView ? "visible" : "hidden"}
       >
-        <GradientBoxMotion
-          id="grid-cell-1"
+        <RightBorderLine />
+        <LeftBorderLine />
+        <Box component="div" sx={(theme) => ({})}>
+          <GradientText
+            sx={(theme) => ({
+              // width: 750,
+              fontSize: 150,
+              fontFamily: "Bebasneue",
+              fontWeight: "bolder",
+              // transform: 'scaleX(2)'
+            })}
+          >
+            {/* Code. Create. Conquer. Exploring Tomorrow’s Tech, and Riding Through Life with Code & Passion! */}
+            Bonjour !
+          </GradientText>
+        </Box>
+        <Box
+          component="div"
           sx={(theme) => ({
-            // gridColumn: "span 2"
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            gap: 10,
+            // flexDirection: 'column',
+            alignItems: "center",
+            justifyContent: "center",
           })}
         >
-          <Typography>Hello</Typography>
-        </GradientBoxMotion>
-        <GradientBoxMotion
-          id="grid-cell-2"
-          sx={(theme) => ({
-            gridColumn: "span 3",
-          })}
-        >
-          <Typography>Hello</Typography>
-        </GradientBoxMotion>
-        <GradientBoxMotion
-          id="grid-cell-3"
-          sx={(theme) => ({
-            gridRow: "span 3",
-          })}
-        >
-          <Typography>Hello</Typography>
-        </GradientBoxMotion>
-        <GradientBoxMotion
-          id="grid-cell-4"
-          sx={(theme) => ({
-            gridColumn: "span 2",
-          })}
-        >
-          <Typography>Hello</Typography>
-        </GradientBoxMotion>
-        <GradientBoxMotion
-          id="grid-cell-5"
-          sx={(theme) => ({
-            gridColumn: "span 2",
-          })}
-        >
-          <Typography>Hello</Typography>
-        </GradientBoxMotion>
-        <GradientBoxMotion
-          id="grid-cell-6"
-          sx={(theme) => ({
-            gridColumn: "span 3",
-          })}
-        >
-          <Typography>Hello</Typography>
-        </GradientBoxMotion>
-        <GradientBoxMotion
-          id="grid-cell-7"
-          sx={(theme) => ({
-            // gridColumn: "span 2",
-          })}
-        >
-          <Typography>Hello</Typography>
-        </GradientBoxMotion>
+          <Box
+            component="div"
+            sx={(theme) => ({
+              position: "relative",
+              width: 320,
+              height: 320,
+              flexShrink: 0,
+              background: "#262626",
+              borderRadius: 50,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            })}
+          >
+            <Box
+              component="img"
+              sx={(theme) => ({
+                width: 280,
+                height: 280,
+                // position: "absolute",
+                // top: 35,
+                // left: 35,
+                border: "1px solid #4c4c4c",
+                borderRadius: 50,
+              })}
+              src={me}
+            />
+            {/*  */}
+          </Box>
+          <Box
+            component="div"
+            sx={(theme) => ({
+              // width: 650
+            })}
+          >
+            <Typography
+              sx={(theme) => ({
+                color: "white",
+                fontFamily: "Bebasneue",
+                fontSize: 25,
+                width: 800,
+              })}
+            >
+              Hey There,&nbsp;&nbsp;
+              <Reveal inline>
+                <Typography
+                  component="span"
+                  sx={(theme) => ({
+                    color: "homePage.navButtons",
+                    fontFamily: "inherit",
+                    fontSize: "inherit",
+                    fontWeight: "bolder",
+                    letterSpacing: 1
+                  })}
+                >
+                  I'm Leela Krishna.
+                </Typography>
+              </Reveal>
+              &nbsp;&nbsp;I'm a Full Stack Developer based in Vizag, India.
+              Passionate about crafting dynamic and user-friendly web
+              applications. Continouosly seeking the latest trends, I enjoy
+              taking on new challenges and learning new technologies. Beyond
+              coding, I’m always eager to explore new technologies, especially
+              Web3 and blockchain development. In my free time, you’ll find me
+              playing basketball with friends, binge-watching Netflix series, or
+              going on bike rides to unwind. Let’s connect and build something
+              amazing! 🚀
+            </Typography>
+          </Box>
+          <Box component="div" sx={(theme) => ({
+
+          })}>
+
+          </Box>
+        </Box>
+        <Box component="div" sx={(theme) => ({
+
+        })}>
+
+        </Box>
       </BoxMotion>
     </Layout>
   );
