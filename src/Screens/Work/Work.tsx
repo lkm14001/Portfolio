c93093aa727c89be65a13c5b628d7f598fd87342
Leela Krishna <krishnaleela35@gmail.com>
2025-02-15 14:12:20 +0530

    Removed carousel

--- src/Screens/Work/Work.tsx
+++ src/Screens/Work/Work.tsx
@@ -1,23 +1,13 @@
-import { useState } from "react";
-import Carousel from "../../components/Carousel/Carousel";
 import Layout from "../Layout/Layout";
 import { Box } from "@mui/material";
 
 const Work = () => {
-  const projects = ['components will be created in next commit'];
-
-  const [activeProject, setActiveProject] = useState<number>(0);
-
-  const handleProjectChange = (itemNumber: number) => {
-    setActiveProject(itemNumber);
-  };
 
   return (
     <Layout
       sx={{
-        // width: "100vw",
-        // height: "100vh",
         position: "relative",
+        boxSizing: "border-box",
       }}
       id="mywork"
     >
@@ -29,37 +19,10 @@ const Work = () => {
           boxSizing: "border-box",
           backgroundColor: "homePage.background",
           zIndex: 10,
-          p: 5,
+          // p: 1,
         })}
       >
-        <Carousel
-          items={projects}
-          onChange={handleProjectChange}
-          selectedItem={activeProject}
-          containerStyle={(theme) => ({
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            height: 300,
-            width: "100%",
-            gap: 5,
-            border: "1px solid white",
-          })}
-          itemStyle={(theme) => ({
-            width: 300,
-          })}
-          selectedItemStyle={(theme) => ({
-            border: '5px solid white',
-          })}
-          navigationColor="white"
-          navigationSize="large"
-          itemAlignment="row"
-          itemGap={5}
-          hoverStyle={(theme) => ({
-            transform: 'scale(1.1)',
-            transition: "all 0.3s ease"
-          })}
-        />
+        
       </Box>
     </Layout>
   );
