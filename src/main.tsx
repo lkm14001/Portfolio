5ad5cb7bab0fd998caf60cc94e9951b48c80ee7d
Leela Krishna <krishnaleela35@gmail.com>
2025-02-17 22:53:34 +0530

    Added Forms with react-hook-froms and toast with react-hot-toast

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,8 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
-
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
